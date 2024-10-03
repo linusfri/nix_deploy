98ff12c939022139e690c8b8f1e1ab928dc87270
Linus <linus.f@protonmail.ch>
2024-10-03 20:51:53 +0200

    Test config

--- flake.nix
+++ flake.nix
@@ -6,25 +6,24 @@
       flake-utils.url = "github:numtide/flake-utils";
     };
   
-    outputs = { self, nixpkgs, flake-utils }:
-      flake-utils.lib.eachDefaultSystem(system:
-        let 
-          pkgs = import nixpkgs { inherit system; };
-        in
-        {
-          packages = rec {
-            nix-deploy = pkgs.rustPlatform.buildRustPackage {
-              pname = "nix_deploy";
-              version = "0.1";
-              cargoLock.lockFile = ./Cargo.lock;
-              src = pkgs.lib.cleanSource ./.;
-              nativeBuildInputs = [
-                pkgs.pkg-config
-              ];
-            };
-
-            default = nix-deploy;
+    outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem(system:
+      let 
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        packages = rec {
+          nix-deploy = pkgs.rustPlatform.buildRustPackage {
+            pname = "nix_deploy";
+            version = "0.1";
+            cargoLock.lockFile = ./Cargo.lock;
+            src = pkgs.lib.cleanSource ./.;
+            nativeBuildInputs = [
+              pkgs.pkg-config
+            ];
           };
-        }
-      );
+
+          default = nix-deploy;
+        };
+      }
+    );
 }
--- src/config.rs
+++ src/config.rs
@@ -4,10 +4,10 @@ pub struct Config {
 
 impl Config {
     pub fn new() -> Self {
-        dotenv::dotenv().ok();
-        let address_and_port =
-            std::env::var("ADDRESS_AND_PORT").expect("ADDRESS_AND_PORT must be set.");
+        // dotenv::dotenv().ok();
+        // let address_and_port =
+        //     std::env::var("ADDRESS_AND_PORT").expect("ADDRESS_AND_PORT must be set.");
 
-        Config { address_and_port }
+        Config { address_and_port: String::from("127.0.0.1:8080") }
     }
 }
